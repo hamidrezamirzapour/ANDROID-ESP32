53517677e4b984ce606201ca42c1a7254d773b5f
hamidrezamirzapour <65387375+hamidrezamirzapour@users.noreply.github.com>
2026-09-22 17:41:12 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'tmas-cache-v1';
+const ASSETS = ['./index.html', './manifest.json', './logo3.jpg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
